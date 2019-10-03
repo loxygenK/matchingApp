35489f5448017abec63f6b2e452a72fe8bc1e541
isso0424 <isso04240129@gmail.com>
2019-10-03 15:32:50 +0900

    setup fireStone (without Add app on fireBase

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       title: "マッチングアプリ(仮)",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("マッチングアプリ(仮)"),
+        ),
+        body: BaseView(),
+      )
     );
   }
-
 }
+//TODO make BaseVIew with StatelessWidget
