c9e1e36c65b60b118764fb9c92d71d01f89dacb2
github nitf <2tf2003@gmail.com>
2019-10-06 23:54:27 +0900

    feat: rename

---
+++ lib/info_display.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class InfoDisplay extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("詳細"),
+        ),
+        body: Text("テスト")
+    );
+  }
+}
