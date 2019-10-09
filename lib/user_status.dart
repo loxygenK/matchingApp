b6cc568198a245b9a03ac38aa2f05d4e0c318c0d
loxygenK <loxygen.k@gmail.com>
2019-10-09 21:40:13 +0900

    [Add] New utility class to save the state of whether the user created room

---
+++ lib/user_status.dart
@@ -0,0 +1,35 @@
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserRoomStatus{
+
+	static void markAsCreatedRoom(String room_name) async {
+
+		SharedPreferences pref = await SharedPreferences.getInstance();
+		await pref.setBool("room_saved", true);
+		await pref.setString("room_name", room_name);
+
+	}
+
+	static void markAsDeletedRoom() async {
+
+		SharedPreferences pref = await SharedPreferences.getInstance();
+		await pref.setBool("room_saved", true);
+		await pref.setString("room_name", "");
+
+	}
+
+	static Future<String> getCreatedRoom() async {
+
+		SharedPreferences pref = await SharedPreferences.getInstance();
+		String room_name;
+
+		if(pref.containsKey("room_saved") && pref.getBool("room_saved")){
+			room_name = pref.getString("room_name");
+		}
+
+		return room_name;
+
+	}
+
+}
